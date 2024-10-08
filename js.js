445cc2c241b9aaa60b9a7d944a109ca0e32337a0
Duc_hoc_code <100946720+duccodedao@users.noreply.github.com>
2024-10-08 11:00:22 +0700

    Update js.js

--- js.js
+++ js.js
@@ -168,8 +168,7 @@ var maintenanceStartTime = new Date("2025-05-15T06:00:00").getTime();
     } else {
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
 
       document.getElementById("days").innerHTML = days;
       document.getElementById("hours").innerHTML = hours;
