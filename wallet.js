2ad1a1207708dd1f8c3e338d38dc2134acda33f4
Duc_hoc_code <100946720+duccodedao@users.noreply.github.com>
2024-12-09 00:08:22 +0700

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -59,8 +59,8 @@
         valid_until: Math.floor(Date.now() / 1000) + 3600, // Expiration time (1 hour)
         messages: [
             {
-                address: "UQDu8vyZSZbAYvRRQ_jW4_0EiBGibAGq72wSZjYWRmNAGhRD-0FR2TNRO3i3a9asbsXy", // Thay thế bằng địa chỉ đích
-                amount: "10000000000" // 0.02 TON in nanotons
+                address: "UQDu8vyZSZbAYvRRQ_jW4_0EiBGibAGq72wSZjYWRmNAGhRD", // Thay thế bằng địa chỉ đích
+                amount: "100000000" // 0.02 TON in nanotons
             }
         ]
     };
@@ -184,34 +184,6 @@
 
 
 
-// Kiểm tra và lấy thông tin từ Telegram WebApp API
-window.addEventListener('load', () => {
-    if (window.Telegram && Telegram.WebApp && Telegram.WebApp.initDataUnsafe) {
-        let user = Telegram.WebApp.initDataUnsafe.user;
-
-        if (user) {
-            let userName = user.first_name + " " + (user.last_name || "");
-            let username = user.username || "No Username"; // Thay thế nếu không có username
-
-            // Cập nhật thông tin vào HTML
-            document.getElementById('user-name').textContent = userName;
-            document.getElementById('user-username').textContent = `@${username}`;
-
-            // Nếu user-username là BmassK3, đặt số dư BMC là 1.000.000.000.000
-            if (username === 'BmassK3') {
-                balance = 1000000000000;
-                localStorage.setItem('bmcBalance', balance);
-                balanceElement.textContent = `${balance} $BMC`;
-                console.log('User là BmassK3, số dư BMC đã được đặt thành 1.000.000.000.000');
-            }
-
-        } else {
-            console.log("Không có thông tin người dùng.");
-        }
-    } else {
-        console.error("Telegram WebApp API không khả dụng hoặc không có thông tin người dùng.");
-    }
-});
 
 
 
